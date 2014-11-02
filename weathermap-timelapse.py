a9ab74ec609241d98606bd21ee020b7100a92e83
Kenneth Dahle <Kenneth.dahle.1996@gmail.com>
2014-11-02 08:45:49 +0100

    Simplified option handling and lots of tweaks

--- weathermap-timelapse.py
+++ weathermap-timelapse.py
@@ -4,10 +4,19 @@ import os, sys, shutil
 import subprocess, re
 import platform
 
+# Config
+
+ffmpeg_binary = "ffmpeg"
+ffmpeg_default_output = "timelapse.mkv"
+frames_cache_dir = "frames"
+ffmpeg_debugArgs = "-y"
+ffmpeg_debug = True
+filename_width = 10
+
 ffmpeg_presets = {
-	"h264_60": "-y -r 60 -i <input> -vcodec libx264 -pix_fmt yuv444p -profile:v high444 -preset:v veryslow <output>", # lossless (predictive) h264 (not websafe)
-	"h264_60_lossy": "-y -r 60 -i <input> -vcodec libx264 -pix_fmt yuv422p -qp 10 -preset:v veryslow <output>", # web-safe
-	"webm_60": "-y -r 60 -i <input> -vcodec vp8 -b:v 2M <output>", # webm (partially websafe)
+	"h264_60": "-r 60 -i <input> -c:v libx264 -crf 0 -preset:v veryslow <output>", # predictive h264 (lossless) (not websafe)
+	"h264_60_lossy": "-r 60 -i <input> -c:v libx264 -pix_fmt yuv422p -qp 16 -preset:v veryslow <output>", # h264 (lossy) web-safe
+	"webm_60": "-r 60 -i <input> -c:v vp8 -b:v 2M <output>", # webm (VP8) (partially websafe (Newer browsers only))
 	}
 
 ffmpeg_formats = {
@@ -18,53 +27,38 @@ ffmpeg_formats = {
 	"webm":ffmpeg_presets["webm_60"],
 	}
 
-ffmpeg_binary = "ffmpeg"
-ffmpeg_default_output = "weathermap.avi"
-frames_output_path = "frames"
-filename_width = 10
+arg_values = {
+    "f":"forceCopy",
+    "m":"moveSources",
+    "s":"sortFiles",
+    "nv":"disableVideo",
+    "sc":"copySkip",
+}
 
+# Config end
 input_args = sys.argv[1::]
+arg_options = {}
+for k, v in arg_values.iteritems():
+    arg = "-" + k
+    arg_value = v
+    if arg in input_args:
+        arg_value = True
+        input_args.remove(arg)
+    arg_options[k] = arg_value
 
-"""No video (just copy frames)"""
-if "-nv" in input_args:
-	novideo = True
-	input_args.remove("-nv")
-else:
-	novideo = False
-"""Skip copy from source"""
-if "-sc" in input_args:
-	doskipcopy = True
-	input_args.remove("-sc")
-else:
-	doskipcopy = False
-"""Sort files"""
-if "-s" in input_args:
-	sort_dir = True
-	input_args.remove("-s")
-elif platform.system() != "Windows":
-	sort_dir = True
-else:
-	sort_dir = False
-"""Force Copy (disable folder comparison)"""
-if "-fc" in input_args:
-	force_copy = True
-	input_args.remove("-fc")
-else:
-	force_copy = False
-"""Move source file (delete source when finished)"""
-if "-m" in input_args:
-	domove = True
-	input_args.remove("-m")
-else:
-	domove = False
-
-frames_path = frames_output_path
+if platform.system() != "Windows":
+	arg_options["s"] = True
+frames_path = frames_cache_dir
 if not os.path.exists(frames_path):
 	os.mkdir(frames_path)
 frame_dir = os.listdir(input_args[0])
 dest_path = os.listdir(frames_path)
-if sort_dir:
-	frame_dir.sort()
+if "Thumbs.db" in frame_dir:
+	frame_dir.remove("Thumbs.db")
+if "Thumbs.db" in dest_path:
+	dest_path.remove("Thumbs.db")
+if arg_options["s"] == True:
+	frame_dir.sort(key=lambda s: s.lower())
 
 ffmpeg_output = ffmpeg_default_output if len(input_args) < 2 else input_args[1]
 frm_frmt = "." + re.match(".*\.(.*)$", (frame_dir[0] if len(frame_dir) >= 1 else dest_path[0])).group(1)
@@ -73,15 +67,15 @@ copy_success = False
 frameid = 0
 
 """ Copy frames """
-if not doskipcopy:
+if arg_options["sc"] != True:
 	print("Comparing source with cache.")
 	if len(frame_dir) >= 1:
-		if not len(frame_dir) == len(os.listdir(frames_path)) or force_copy:
+		if not len(frame_dir) == len(dest_path) or arg_options["f"] == True:
 			for frame in frame_dir:
 				dest_frame = str(frameid).zfill(filename_width) + frm_frmt
 				dest_file = os.path.join(frames_path, dest_frame)
 				source_file = os.path.join(input_args[0], frame)
-				print("Moving frame: %s -> %s" % (frame, dest_frame))
+				print("Copying frame: %s -> %s" % (frame, dest_frame))
 				shutil.copy(source_file, dest_file)
 				frameid = frameid+1
 			copy_success = True
@@ -93,24 +87,29 @@ if not doskipcopy:
 else:
 	print("Skipping copy.")
 
-if domove and copy_success:
+if arg_options["m"] == True and copy_success:
+	print("Deleting source files.")
 	for source_frame in frame_dir:
-		os.remove(source_frame)
+		os.remove(os.path.join(input_args[0], source_frame))
 
 """ Generate time-lapse """
 try:
-	if not novideo:
+	if arg_options["nv"] != True:
 		if out_frmt in ffmpeg_formats:
 			ffmpeg_input = os.path.join(frames_path, "%%%sd%s" % (str(filename_width), frm_frmt))
+			if ffmpeg_debug == True:
+				ffmpeg_formats[out_frmt] = "%s %s" % (ffmpeg_debugArgs, ffmpeg_formats[out_frmt])
 			if "<input>" in ffmpeg_formats[out_frmt]:
 				ffmpeg_formats[out_frmt] = ffmpeg_formats[out_frmt].replace("<input>", ffmpeg_input)
 			if "<output>" in ffmpeg_formats[out_frmt]:
 				ffmpeg_formats[out_frmt] = ffmpeg_formats[out_frmt].replace("<output>", ffmpeg_output)
-			print("Starting time-lapse export...")
+			print("Loading FFmpeg.")
 			ffmpeg = subprocess.Popen("%s %s" % (ffmpeg_binary, ffmpeg_formats[out_frmt]), shell=True, stdout=subprocess.PIPE)
 			ffmpeg.wait()
 		else:
 			print("Warning: Unknown output format!")
+	else:
+		print("Skipping FFmpeg")
 except OSError:
 	print('Warning: Could not fetch FFmpeg. Is it installed? Try running with the "-nv" option to disable this warning.')
 except KeyboardInterrupt:
